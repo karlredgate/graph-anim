ac460eb52c644d8a0ef26b532828c6f639dc18bc
Karl Redgate <Karl.Redgate@gmail.com>
2017-01-22 10:52:26 -0500

    add ann skel

---
+++ ANN.h
@@ -0,0 +1,67 @@
+
+/*
+ * Copyright (c) 2012 Karl N. Redgate
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <string>
+
+#include "Graph.h"
+
+#ifndef _NEURALNET_H_
+#define _NEURALNET_H_
+
+/**
+ */
+class Neuron : public Vertex {
+public:
+    Neuron();
+    virtual ~Neuron();
+};
+
+/**
+ * dendrite is incoming, axon would be outgoing.
+ * But an axon output should go to multiple and there is a single axon
+ */
+class Dendrite : public Edge {
+public:
+    Dendrite();
+    Dendrite( Neuron * );
+    virtual ~Dendrite();
+};
+
+/**
+ */
+class Layer : public Graph {
+public:
+    bool connect( Layer * );
+    Layer();
+    virtual ~Layer();
+};
+
+/**
+ */
+class NeuralNet : public Graph {
+};
+
+#endif
+
+/* vim: set autoindent expandtab sw=4 : */
